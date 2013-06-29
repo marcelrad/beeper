813da0b05a71a7853feee5674c6ec79996e972ba
Jochen Goertler <lyzrd17@gmail.com>
2013-06-29 14:42:43 +0200

    Refactoring.

---
+++ SoundOutput.h
@@ -0,0 +1,98 @@
+#include <math.h>
+#include <vector>
+#include <iostream>
+// adjust header for linux
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <chrono>
+#include <thread>
+
+struct Stream
+{
+	Stream(size_t sample_rate, float duration) : 
+		sample_rate(sample_rate),
+		duration(duration),
+       		samples(duration * sample_rate) {};
+
+	void generate_sine(int frequency) {
+		// create a sine wave. basically a tone. :)
+		for(size_t i = 0; i < samples.size(); ++i) { 
+			samples[i] = volume * sin((2.f * M_PI * float(frequency)) / sample_rate * i); 
+		}
+	}
+	
+	void modulate_sine(int frequency) {
+		// multiplicate the existing sound with a sinus wave.
+		for(size_t i = 0; i <  samples.size(); ++i) {
+			samples[i] *=  sin((2.f * M_PI * float(frequency)) / sample_rate * i);
+		}
+	}
+
+	size_t size() const {
+		return sizeof(uint16_t)*samples.size();
+	}
+
+	const uint16_t* data() const {
+		return samples.data();
+	}
+
+	size_t const sample_rate;
+	float const duration;
+	size_t const volume = 32760;
+	
+	private:
+		std::vector<uint16_t> samples;
+};
+
+struct SoundOutput
+{
+	size_t const sample_rate;
+		
+	SoundOutput(size_t sample_rate) : sample_rate(sample_rate) {
+		ALCdevice *dev = NULL;
+		ALCcontext *ctx = NULL;
+
+		const char *defname = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
+		std::cout << "Default device: " << defname << std::endl;
+
+		dev = alcOpenDevice(defname);
+		ctx = alcCreateContext(dev, NULL);
+		alcMakeContextCurrent(ctx);
+	}
+
+	~SoundOutput() {
+		ALCdevice *dev = NULL;
+		ALCcontext *ctx = NULL;
+		ctx = alcGetCurrentContext();
+		dev = alcGetContextsDevice(ctx);
+
+		alcMakeContextCurrent(NULL);
+		alcDestroyContext(ctx);
+		alcCloseDevice(dev);
+		std::cout << "Device closed." << std::endl;
+	}
+
+	void play(Stream stream) {
+		
+		/* Create buffer to store samples */
+		ALuint buf;
+		alGenBuffers(1, &buf);
+
+		/* Download buffer to OpenAL */
+		alBufferData(buf, AL_FORMAT_MONO16, stream.data(), stream.size(), sample_rate);
+
+		/* Set-up sound source and play buffer */
+		ALuint src = 0;
+		alGenSources(1, &src);
+		alSourcei(src, AL_BUFFER, buf);
+		alSourcePlay(src);
+
+		/* While sound is playing, sleep */
+		std::chrono::milliseconds duration( int(stream.duration * 1000) );
+		std::this_thread::sleep_for( duration );
+	}
+
+	void operator()(Stream stream) {
+		play(stream);
+	}
+};
--- beeper.cxx
+++ beeper.cxx
@@ -1,87 +1,20 @@
-#include <math.h>
-#include <vector>
-#include <iostream>
-#include <OpenAL/al.h>
-#include <OpenAL/alc.h>
-#include <chrono>
-#include <thread>
+#include "SoundOutput.h"
 
-struct SoundOutput
-{
-
-	void init() {
-		ALCdevice *dev = NULL;
-		ALCcontext *ctx = NULL;
-
-		const char *defname = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
-		std::cout << "Default device: " << defname << std::endl;
-
-		dev = alcOpenDevice(defname);
-		ctx = alcCreateContext(dev, NULL);
-		alcMakeContextCurrent(ctx);
-	}
-
-	void exit() {
-		ALCdevice *dev = NULL;
-		ALCcontext *ctx = NULL;
-		ctx = alcGetCurrentContext();
-		dev = alcGetContextsDevice(ctx);
-
-		alcMakeContextCurrent(NULL);
-		alcDestroyContext(ctx);
-		alcCloseDevice(dev);
-	}
-
-	void play_tone(float frequency, int seconds) {
-		
-		size_t const sample_rate = 22050; 
-		size_t const volume      = 32760; 
-		
-		std::vector<uint16_t> samples(seconds * sample_rate);
-		
-		for(std::size_t i = 0; i < samples.size(); ++i) { 
-			samples[i] = volume * sin((2.f * M_PI * frequency) / sample_rate * i); 
-		}
-
-		/* Create buffer to store samples */
-		ALuint buf;
-		alGenBuffers(1, &buf);
-
-		/* Download buffer to OpenAL */
-		alBufferData(buf, AL_FORMAT_MONO16, samples.data(), sizeof(uint16_t) * samples.size(), sample_rate);
-
-		/* Set-up sound source and play buffer */
-		ALuint src = 0;
-		alGenSources(1, &src);
-		alSourcei(src, AL_BUFFER, buf);
-		alSourcePlay(src);
-
-		/* While sound is playing, sleep */
-		std::chrono::milliseconds dura( seconds * 1000 );
-		std::this_thread::sleep_for( dura );
-	}
-
-	void operator()(float frequency, int seconds) {
-		play_tone(frequency, seconds);
-	}
-};
 
 int main(int argc, char* argv[]) {
 	
-	SoundOutput out;
-	out.init();
+	// sample rate
+	int sr = 22050;
 
-	char key;
-	while (key != 'q') {
-		std::cin >> key;
-		std::cout << "Key pressed: [" << key << "]" << std::endl;
+	SoundOutput out(sr);
 
-		if (key == 'a') {std::thread t1(out, 440.f, 2); t1.detach();}
-		else if (key == 's') {std::thread t2(out, 220.f, 2); t2.detach();}
-		else {std::cout << "Not a valid key. Please try again." << std::endl;}
+	// stream with duration of 1 second
+	for (int i = 1; i <= 5; ++i) {
+		Stream as(sr, 2);
+		as.generate_sine(440);
+		as.modulate_sine(i);
+		out.play(as);
 	}
-	std::cout << "I'm quitting now..." << std::endl;
 
-	out.exit();
 	return 0;
 }
